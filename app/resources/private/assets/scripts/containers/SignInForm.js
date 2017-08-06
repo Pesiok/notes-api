cb1d6ee118259e3bcc940567ccab5a7d87f803a4
Grzegorz Bielski <pesiok@gmail.com>
2017-08-06 19:05:04 +0200

    added sign in validation

--- app/resources/private/assets/scripts/containers/SignInForm.js
+++ app/resources/private/assets/scripts/containers/SignInForm.js
@@ -1,56 +1,112 @@
 import React, { Component } from 'react';
-import Input from './../components/Input';
-
 
 export default class SignInForm extends Component {
     constructor() {
         super();
         this.state = { 
-            name: '',
-            password: '',
-            passwordConf: ''
+            name: { value: '', isValid: true },
+            password: { value: '', isValid: true },
+            passwordConf: { value: '', isValid: true }
         };
+    }
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    _validate(value, name) {
+        let validity = this.state[name].isValid;
+        switch(name) {
+            case 'name': {
+                validity = (value.length < 3 || value.length === 0) ? false : true;
+                break;
+            }
+            case 'password': {
+                validity =  (value.length < 6 || value.length === 0) ? false : true;
+                break;
+            }
+            case 'passwordConf': {
+                validity = (value !== this.state.password.value) ? false : true;
+                break;
+            }
+            default: break;
+        }
+        return validity;
+    }
+    
+    checkValidity(value, name, checktLastState = false) {
+        const isValid = checktLastState ? true : this._validate(value, name);
+        this.setState(state => {
+            return Object.assign({}, state, {
+                [name]: { value, isValid }
+            })
+        });
+
+        return isValid;
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        console.log(this.state);
+
+        // validate onSubmit
+        const areValid = Object.keys(this.state).map(key => {
+            return this.checkValidity(this.state[key].value, key);
+        });
+        // stop if some of the inputs has invalid value
+        if(areValid.some(input => input === true)) return false;
+
+        // otherwise send request
+        // some redux saga mombo-jumbo
+
     }
 
-    handleChange(event, type) {
+    handleBlur(event, name) {
         const value = event.target.value;
-        this.setState(Object.assign({}, this.state, {
-             [type]: value 
-        }));
+
+        // validate onBlur by default
+        this.checkValidity(value, name);
+    } 
+
+    handleChange(event, name) {
+        const value = event.target.value;
+        
+        // trigger onChange validation after some handler validated to false
+        this.checkValidity(value, name, this.state[name].isValid);
     }
 
     render() {
+
         return (
-            <form onSubmit={this.handleSubmit}>
-                <Input 
-                    name="Name" 
-                    type="text" 
-                    value={this.state.name}
-                    valueSrc ="name" 
-                    handler={this.handleChange}
-                />
-                <Input
-                    name="Password"
-                    type="password"
-                    value={this.state.password}
-                    valueSrc="password"
-                    handler={this.handleChange}
-                />
-                <Input 
-                    name="Confirm password"
-                    type="password"
-                    value={this.state.passwordConf}
-                    valueSrc="passwordConf"
-                    handler={this.handleChange}
-                />
+            <form onSubmit={event => this.handleSubmit(event)}>
+                <label> Username
+                    <input 
+                        type="text"
+                        value={this.state.name.value}
+                        onChange={event => this.handleChange(event, 'name')}
+                        onBlur={event => this.handleBlur(event, 'name')}
+                    />
+                    <span style={ { display: this.state.name.isValid ? 'none' : 'inherit' } }>
+                        Your username must be at least 3 characters long.
+                    </span>
+                </label>
+                <label> Password
+                    <input 
+                        type="password"
+                        value={this.state.password.value}
+                        onChange={event => this.handleChange(event, 'password')}
+                        onBlur={event => this.handleBlur(event, 'password')}
+                    />
+                     <span style={ { display: this.state.password.isValid ? 'none' : 'inherit' } }>
+                        Your password must be at least 6 characters long.
+                    </span>
+                </label>
+                <label> Confirm Password
+                    <input 
+                        type="password"
+                        value={this.state.passwordConf.value}
+                        onChange={event => this.handleChange(event, 'passwordConf')}
+                        onBlur={event => this.handleBlur(event, 'passwordConf')}
+                    />
+                     <span style={ { display: this.state.passwordConf.isValid ? 'none' : 'inherit' } }>
+                        Password does not match the confirm password
+                    </span>
+                </label>
                 <button type="submit">Submit</button>
             </form>
         );
