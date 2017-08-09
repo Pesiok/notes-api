9184c1612909abcb53e350a688f20426c19877ec
Grzegorz Bielski <pesiok@gmail.com>
2017-08-09 19:58:04 +0200

    added find user route

--- app/tests/user.test.js
+++ app/tests/user.test.js
@@ -12,7 +12,7 @@ describe('User: ', () => {
     beforeEach(populateNotes);
 
     describe('GET /api/users/me', () => {
-        it('should return authendicated user with only id and name', async () => {
+        it('should return authenticated user with only id and name', async () => {
             const response = await chai.request(app)
                 .get('/api/users/me')
                 .set('x-auth', users[0].tokens[0].token);
@@ -32,6 +32,27 @@ describe('User: ', () => {
                     .set('x-auth', users[0].tokens[0].token);
             } catch ({ response }) {
                 expect(response).to.have.status(401);
+            }
+        });
+    });
+
+    describe('GET /api/users/find/:name', () => {
+        it('should return a user if found', async () => {
+            const response = await chai.request(app)
+                .get(`/api/users/find/${users[0].name}`);
+            expect(response).to.have.status(200);
+
+            const user = response.body;
+            expect(user._id).to.equal(users[0]._id.toHexString());
+            expect(user.name).to.equal(users[0].name);
+        });
+
+        it('shouldn\'t return a user if not found', async () => {
+            try {
+                const response = await chai.request(app)
+                    .get('/api/users/find/randomlogin123');
+            } catch({ response }) {
+                expect(response).to.have.status(404);
             }
         });
     });
